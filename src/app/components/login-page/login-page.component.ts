adea23e2811608536d57729501f3b43693aa0b57
wasja9 <wasja@yandex.ru>
2020-11-16 02:26:12 +0300

    add file

---
+++ src/app/components/login-page/login-page.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import {FormGroup, FormControl, Validators} from "@angular/forms";
+import {AuthService} from "../shared/services/auth.services";
+
+@Component({
+  selector: 'app-login-page',
+  templateUrl: './login-page.component.html',
+  styleUrls: ['./login-page.component.css']
+})
+export class LoginPageComponent implements OnInit {
+form:FormGroup;
+
+  constructor(private auth:AuthService) { }
+
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      login: FormControl(null, [Validators.required, Validators.email]),
+      password: FormControl(null, [Validators.required, Validators.minLength(2)])
+    })
+
+  }
+  onSubmit(){
+    const  user={
+      login: this.form.value.login,
+      password: this.form.value.password,
+    }
+    this.auth.login(user).subscribe(
+      ()=>console.log('Login success'),
+      error=>{
+        console.warn(error)
+      }
+    )
+
+  }
+
+}
---
+++ src/app/components/shared/layouts/site-lajout/site-lajout.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-site-lajout',
+  templateUrl: './site-lajout.component.html',
+  styleUrls: ['./site-lajout.component.css']
+})
+export class SiteLajoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
