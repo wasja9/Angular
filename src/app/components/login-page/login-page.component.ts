83b4c893ab1174f75011dcdd24572dedbabbcf93
wasja9 <wasja@yandex.ru>
2020-11-19 23:05:03 +0300

    add setting

--- src/app/components/login-page/login-page.component.ts
+++ src/app/components/login-page/login-page.component.ts
@@ -1,37 +1,66 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, OnDestroy} from '@angular/core';
 import {FormGroup, FormControl, Validators} from "@angular/forms";
 import {AuthService} from "../shared/services/auth.services";
+import {Subscription} from "rxjs";
+import {ActivatedRoute, Router, Params} from "@angular/router";
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.css']
 })
-export class LoginPageComponent implements OnInit {
+export class LoginPageComponent implements OnInit, OnDestroy {
 form:FormGroup;
+aSub: Subscription;
 
-  constructor(){}//private auth:AuthService) { }
+  constructor(private auth:AuthService,
+  private  router:Router,
+              private route: ActivatedRoute
+  ) { }
+
 
   ngOnInit(): void {
     this.form = new FormGroup( {
       login: new FormControl(null, [Validators.required, Validators.email]),
       password:new FormControl(null, [Validators.required, Validators.minLength(2)])
     })
-
+    this.route.queryParams.subscribe((params: Params)=>{});
   }
   onSubmit(){
-    /* */
-    const  user={
-      login: this.form.value.login,
-      password: this.form.value.password,
+    this.aSub=this.auth.login(this.form.value).subscribe(
+      ()=>this.router.navigate(['/overview']),
+      error=>{
+        console.warn(error)
+        this.form.enable()
+
     }
+
+
+    )
+    /* */
+   // const  user={
+   //   login: this.form.value.login,
+   //   password: this.form.value.password,
+   // }
    // this.auth.login(user).subscribe(
    //   ()=>console.log('Login success'),
-   //   error=>{
-    //    console.warn(error)
-   //   }
-   // )
+  //    error=>{
+  //      console.warn(error)
+  //    }
+  //  )
 
   }
+
+ngOnDestroy(){
+    if (this.aSub){
+      this.aSub.unsubscribe();
+    }
+}
+
+//constructor(private auth: AuthService,
+//private  router: Router,
+//private  route: ActivatedRoute){
+
+//}
 
 }
--- src/app/components/shared/services/auth.services.ts
+++ src/app/components/shared/services/auth.services.ts
@@ -2,10 +2,13 @@
 import {Injectable} from '@angular/core'
 import {HttpClient} from "@angular/common/http";
 
-import {User} from "./interfaces";
+import {User} from "../interfaces";
 import {Observable} from "rxjs";
 import {tap} from 'rxjs/operators'
 
+//@Injectable
+
+//providedIn: 'root';
 export class AuthService{
 
  private token = null;
