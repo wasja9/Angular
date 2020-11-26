39ea4293a769b03dd8aa57212aea91534999be2f
wasja9 <wasja@yandex.ru>
2020-11-26 18:53:42 +0300

    errors send

--- src/app/components/test/test.component.ts
+++ src/app/components/test/test.component.ts
@@ -5,6 +5,7 @@ import {FormsModule} from '@angular/forms';
 import {Injectable} from "@angular/core";
 import {Observable} from "rxjs";
 import {Response} from "selenium-webdriver/http";
+import {Token} from "@angular/compiler";
 //import {Http, Response} from '@angular/http'
 
 @Injectable()
@@ -18,6 +19,7 @@ export class TestComponent { //implements OnInit {
 
   test:string="dddddddddddddd";
   tss:string='ggg';
+  data:any;
 
   userName:string="";
   response:any;
@@ -31,17 +33,17 @@ export class TestComponent { //implements OnInit {
   get_s(){
    // get_s():Observable<>{
     console.log("GET1: ");
-    //this.test=this.test+'fr'
     console.log("GET2: ");
-   //return this.http.get('http://localhost:4200/')
 
-    this.http.get('http://localhost:8080/users')
+    //return this.http.get('http://localhost:8080/users/')
+    this.http.get('http://localhost:8080/users/')
       .subscribe(data=>{
+        console.log("GET3: ");
         console.log(data);
       })
    //return this.http.get<{token:string}>('http://localhost:8080/users/')
 
-    console.log("GET3: ");
+    console.log("GET4: ");
 
     //this.http.get<{token:string}>('api.github.com/users')
     //api.github.com/users/seeschweiler
@@ -72,7 +74,7 @@ export class TestComponent { //implements OnInit {
     //postData(user: User){
     //user:string =
     this.user='{"firstName":"sss","lastName":"sss","login":"sss","password":"123"}';
-    this.http.post('http://localhost:8080/users/', this.user);
+    return this.http.post('http://localhost:8080/users/', this.user);
 
     console.log("ADD2: ");
      // .map((response: Response)=>response.json())
