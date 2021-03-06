95820f0722de7c13149361a6d6e9dc75dc7332e4
wasja9 <wasja@yandex.ru>
2021-03-06 19:15:59 +0300

    shrift

--- src/app/components/brn-tree/brn-tree.component.ts
+++ src/app/components/brn-tree/brn-tree.component.ts
@@ -13,7 +13,7 @@ export class BrnTreeComponent  {
   DT:any;
   DT_SORT:any;
   TokenService:string="NWn7lcGfSv";
-  projict_temp:string="";
+  public projict_temp:string="";
   myAr=[[]];
   myArT=[];
 
@@ -34,32 +34,29 @@ export class BrnTreeComponent  {
 ////////////////////////////////////////////////////////////////////////////////////////////
         //Находим корневую запись и записываем в массив
         for (var i = 0; i < Number(this.DT.length); ++i) {
-          if (this.DT[i].id==this.DT[i].idNode){
-            this.myArT[0]=this.DT[i];
-            this.myAr[0] = this.myArT;
+          if (this.DT[i].id==this.DT[i].idNode){//Ищем корень дерева проекта
+            this.myArT[0]=this.DT[i];//Добавляем его в одномерный массив
+            this.myAr[0] = this.myArT;//Добавляем одномерный массив в многомерный
           }
         }
       //сортировать по остальным полям
         let k:number=0;
         let m:number=0;
         let end_wh:boolean=true;
-        let end_temp:boolean=true;
-        while (end_wh){
-          end_wh=true;
-          end_temp=true;
+        while (end_wh){//Цикл по данным, пока находит хоть одного наследника(chtild)
+          end_wh=false;//Окончание цикла если не нашол нового элемента в массиве
+          this.myArT=[];//Обнуление массива
+          m=0;//Коэффициент элемента в вложенном массиве
           for (var j = 0; j < this.myAr[k].length; ++j) {
-            m=0;
-            this.myArT=[];
               for (var i = 0; i < Number(this.DT.length); ++i) {
                 if (this.myAr[k][j].id==this.DT[i].idRoot){
-                  this.myArT[m]=this.DT[i];
-                  this.myAr[k+1] = this.myArT;
+                  this.myArT[m]=this.DT[i];//Добавляем его в одномерный массив
+                  this.myAr[k+1] = this.myArT;//Добавляем одномерный массив в многомерный
                   m++;
-                  end_temp=false;
+                  end_wh=true;//если нашол новый элемента в массиве запустить продолжение цикла
                 }
               }
           }
-          if (! end_temp){end_wh=true;} else{end_wh=false}
           console.log(this.myAr);
           k++;
         }
--- src/app/components/teh-process/teh-process.component.ts
+++ src/app/components/teh-process/teh-process.component.ts
@@ -10,19 +10,20 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./teh-process.component.css']
 })
 export class TehProcessComponent{
-  projict_temp:string="";
+  id_pr_t:string="";
+  id_br_t:string="";
+
   DT:any;
   TokenService:string="NWn7lcGfSv";
 
   //
-
-
   constructor(private http:HttpClient, private activatedRoute:ActivatedRoute) {
     this.TokenService=localStorage.getItem('Auth-Token');
 
     this.activatedRoute.queryParams.subscribe(params => {//Принимаем переменную из адресной строку
-      this.projict_temp = params['id'];
-      console.log("ID =: "); console.log(this.projict_temp);
+      this.id_br_t = params['id_br'];
+      this.id_pr_t = params['projict'];
+      console.log("ID =: "); console.log(this.id_pr_t);console.log(this.id_br_t);
     })
 
     this.http.get('http://localhost:8083/ms/?token='+this.TokenService)
